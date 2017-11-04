380f15f207ff9491f44db393ef642f4ac975a7a8
eehong1986 <zhugyjy8@163.com>
2017-11-04 12:43:44 +0800

    使用重定向和用户会话解决刷新时再次提交表单的问题

--- flasky.py
+++ flasky.py
@@ -1,6 +1,6 @@
 #-*- coding:utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -24,12 +24,11 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
