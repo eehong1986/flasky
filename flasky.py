60e5d5312f952cd477f61280f6204900efe77ea9
eehong1986 <zhugyjy8@163.com>
2017-11-06 23:01:49 +0800

    在视图函数中操作数据库

--- flasky.py
+++ flasky.py
@@ -9,14 +9,52 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
 # 默认情况下，flask-wtf会保护表单免受跨站请求伪造攻击，需要为程序设置密钥
 app.config['SECRET_KEY'] = 'HELLOSTRANGER'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# 建立数据库使用的模型
+class Role(db.Model):
+    __tablename__ = 'roles'
+    # role id
+    id = db.Column(db.Integer, primary_key=True)
+    # role name
+    name = db.Column(db.String(64), unique=True)
+    # 建立联系 users代表这个关系的面向对象视角
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    # user id
+    id = db.Column(db.Integer, primary_key=True)
+    # user name
+    username = db.Column(db.String(32), unique=True, index=True)
+    # 此处添加的role_id被定义为外键，就是这个外键建立起了到Role的联系
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
@@ -26,12 +64,20 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have changed your name!")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            # 新用户，加入到数据库中
+            user_role = Role(name='User')
+            user = User(username=form.name.data, role=user_role)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'),
+            known=session.get('known', False), current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
