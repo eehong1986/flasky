2ac81349261a9ff7345125f90fa62f51f837be55
eehong1986 <zhugyjy8@163.com>
2017-11-15 23:34:56 +0800

    引入密码散列和用户认证

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 #-*- coding:utf-8 -*-
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from . import db
+from . import login_manager
 
 # 建立数据库使用的模型
 class Role(db.Model):
@@ -16,14 +19,33 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     # user id
     id = db.Column(db.Integer, primary_key=True)
+    # user email address
+    email = db.Column(db.String(64), unique=True, index=True)
     # user name
     username = db.Column(db.String(32), unique=True, index=True)
+    # user password
+    password_hash = db.Column(db.String(128))
     # 此处添加的role_id被定义为外键，就是这个外键建立起了到Role的联系
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
