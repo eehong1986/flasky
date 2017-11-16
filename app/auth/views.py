0eedb1bca954bbdab85edaaf98227447f664be26
eehong1986 <zhugyjy8@163.com>
2017-11-16 22:03:55 +0800

    加入用户注册功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,8 +4,9 @@ from flask import render_template, session, redirect, url_for, flash, request
 from flask_login import login_required, login_user, logout_user
 
 from . import auth
-from ..models import User
-from .forms import LoginForm
+from ..models import User, Role
+from .. import db
+from .forms import LoginForm,RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -23,6 +24,21 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        role_user = Role.query.filter_by(name='User').first()
+        user = User(email = form.email.data,
+                username = form.username.data,
+                password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
 
 @auth.route('/secret')
 @login_required
