5ca69880eb3524078e59ab5afb0b03730e161969
eehong1986 <zhugyjy8@163.com>
2017-11-28 23:36:56 +0800

    增加分页显示博客文章功能

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from flask import render_template, session, redirect, url_for,current_app
-from flask import flash
+from flask import flash, request
 from flask_login import login_required, current_user
 
 from . import main
@@ -21,14 +21,20 @@ def index():
                     author = current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('main.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    #posts = Post.query.order_by(Post.timestamp.desc()).all()
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+            page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+            error_out=False)
+    posts = pagination.items
     return render_template('index.html', form=form, posts=posts,
-            current_time=datetime.utcnow())
+            pagination=pagination, current_time=datetime.utcnow())
 
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
